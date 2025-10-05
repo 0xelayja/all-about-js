df2f715d73c61c3c94c5d1cadc80cdc37bfffb9c
Miguel Oresca <0xelayja@users.noreply.github.com>
2025-10-06 03:34:08 +0800

    updated function

--- AJS.js
+++ AJS.js
@@ -3,5 +3,6 @@ let usernameAnything;
 
 document.getElementById('onclickBtn').onclick = function (){
   usernameAnything = document.getElementById('input').value;
+  document.getElementById('myH1').textContent = (`Did you know there's actually a reason why you put "${usernameAnything}"?`)
   console.log(usernameAnything);
 }
