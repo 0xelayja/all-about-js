0f120076c0758edf28fad9a982cca5561594eeaa
Miguel Oresca <0xelayja@users.noreply.github.com>
2025-10-06 03:20:44 +0800

    all about js

---
+++ AJS.js
@@ -0,0 +1,7 @@
+
+let usernameAnything;
+
+document.getElementById('onclickBtn').onclick = function (){
+  usernameAnything = document.getElementById('input').value;
+  console.log(usernameAnything);
+}
