e6f976d0028df16ca4c1d0d0234945c47b47e687
Miguel Oresca <0xelayja@users.noreply.github.com>
2025-10-06 11:56:32 +0800

    counting program with html css but this is all about js

--- AJS.js
+++ AJS.js
@@ -1,10 +1,20 @@
-const PI = 3.14
-let radius;
-let circumference;
+const countLabel = document.getElementById('countLabel');
+const increaseBtn = document.getElementById('increaseBtn');
+const resetBtn = document.getElementById('resetBtn');
+const decreaseBtn = document.getElementById('decreaseBtn');
+let count = 0;
 
-document.getElementById('myBtn').onclick = function(){
-  radius = document.getElementById("radius").value;
-  radius = Number(radius);
-  circumference = 2 * PI * radius;
-  document.getElementById('myRadius').textContent = circumference + "cm";
+increaseBtn.onclick = function(){
+  count++;
+  countLabel.textContent = count;
+}
+
+resetBtn.onclick = function(){
+  count = 0;
+  countLabel.textContent = count;
+}
+
+decreaseBtn.onclick = function(){
+  count--;
+  countLabel.textContent = count;
 }
