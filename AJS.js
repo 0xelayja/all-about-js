13b9714c96f274e0493bb425be002a621c181309
Miguel Oresca <0xelayja@users.noreply.github.com>
2025-10-08 12:00:00 +0800

    random math roll a dice

--- AJS.js
+++ AJS.js
@@ -1,11 +1,44 @@
+/*
+const ROLL = document.getElementById('ROLL');
+const label1 = document.getElementById('label1');
+const label2 = document.getElementById('label2');
+const label3 = document.getElementById('label3');
+const max = 100;
+const min = 1;
+let randomNumb1;
+let randomNumb2;
+let randomNumb3;
 
 
-//let randomNum = Math.random(); // math will pick from between 0-1
-//let randomNum = Math.random() * 100; // from 0-100
-//let randomNum = Math.floor(Math.random() * 100) + 1; //always round down + 1-100 because we add + 1
+document.getElementById('ROLL').onclick = function(){
+  randomNumb1 = Math.floor(Math.random() * max) + min; 
+  randomNumb2 = Math.floor(Math.random() * max) + min;
+  randomNumb3 = Math.floor(Math.random() * max) + min;
+  label1.textContent = randomNumb1
+  label2.textContent = randomNumb2
+  label3.textContent = randomNumb3
+}
+  */
 
-const min = 100
-const max = 200
 
-let randomNum = Math.floor(Math.random() * (max - min)) + min; // Number will pick numbers from 50-100
+
+const ROLL = document.getElementById('ROLL');
+const label1 = document.getElementById('label1');
+const label2 = document.getElementById('label2');
+const label3 = document.getElementById('label3');
+const max = 200;
+const min = 100;
+let randomNumb1;
+let randomNumb2;
+let randomNumb3;
+
+
+document.getElementById('ROLL').onclick = function(){
+  randomNumb1 = Math.floor(Math.random() * (max - min)) + min; 
+  randomNumb2 = Math.floor(Math.random() * (max - min)) + min;
+  randomNumb3 = Math.floor(Math.random() * (max - min)) + min;
+  label1.textContent = randomNumb1;
+  label2.textContent = randomNumb2;
+  label3.textContent = randomNumb3;
+}
 
